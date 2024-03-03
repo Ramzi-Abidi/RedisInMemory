7d67de21f40a4f85b361abb199e07e1d26bd077f
Ramzi-Abidi <ramziinfo2001@gmail.com>
2024-03-03 14:21:56 +0100

    feat/add-redis-client: add Redis client to send requests to server

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-const { CreateServer } = require("./server");
+const { RedisClient } = require("./RedisClient");
+const { CreateServer } = require("./Server");
+
+const extractPortFromArg = (arr) => {
+    return Number(arr[3]);
+};
+
+const extractHostFromArg = (arr) => {
+    return arr[1];
+};
 
 const start = () => {
+    // create server
     const root = new CreateServer();
 
+    // create connection / listener to clients
     const server = root.initialize();
 
-    const PORT = root.selectPort(process.argv.splice(2));
+    const client = new RedisClient();
 
-    const HOST = "127.0.0.1";
+    const PORT = extractPortFromArg(process.argv.slice(2));
+    const HOST = extractHostFromArg(process.argv.slice(2));
 
+    // server listens to port
     server.listen(PORT, HOST);
+    console.log(`server listens to port ${PORT}`);
+
+    // initialize client
+    client.initialize(PORT, HOST);
+
 };
 
 start();
